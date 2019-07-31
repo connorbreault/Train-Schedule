78d03f3f86e894b92e6aa562796c26c6733fe034
Spaghetti Jones <connor.breault@comcast.net>
2019-07-31 01:06:22 -0700

    firebase added

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,25 +1,22 @@
-//initialize firebase
-//search button saves data to firebase
-//retrieve data from firebase to update train schedule
-//use moment to get next departure
-
 $(document).ready(function () {
 
+    //append date in head
     var today = moment().format('MM/DD/YYYY')
     $("#dateHead").append(today)
 
+
     //initialize firebase
-    //var config = {
-    //    aapiKey: "AIzaSyCFn_oHxP7oa644rlNjraWhk6ywl96staU",
-    //    authDomain: "train-scheduler-8e77c.firebaseapp.com",
-    //    databaseURL: "https://train-scheduler-8e77c.firebaseio.com",
-    //    projectId: "train-scheduler-8e77c",
-    //    storageBucket: "",
-    //    messagingSenderId: "1046327108985",
-    //    appId: "1:1046327108985:web:23bf75612e809dc5"
-    //};
-    //firebase.initializeApp(config);
-    //var database = firebase.database()
+    var firebaseConfig = {
+        apiKey: "AIzaSyCFn_oHxP7oa644rlNjraWhk6ywl96staU",
+        authDomain: "train-scheduler-8e77c.firebaseapp.com",
+        databaseURL: "https://train-scheduler-8e77c.firebaseio.com",
+        projectId: "train-scheduler-8e77c",
+        storageBucket: "train-scheduler-8e77c.appspot.com",
+        messagingSenderId: "1046327108985",
+        appId: "1:1046327108985:web:23bf75612e809dc5"
+    };
+    firebase.initializeApp(firebaseConfig);
+    var database = firebase.database()
 
 
     //search button click
@@ -29,20 +26,15 @@ $(document).ready(function () {
         var newDestination = $("#newTrainDestination").val()
         var newFirst = $("#newTrainFirst").val()
         var newFrequency = $("#newTrainFrequency").val()
-        var newNext = $("#newTrainNext").val()
-        //var newTrain = {
-        //    Name: newName,
-        //    Destination: newDestination,
-        //    First: newFirst,
-        //    Frequency: newFrequency,
-        //    Next: newNext,
-        //}
-        //database.ref().push(newTrain)
-        console.log("name: " + newName)
-        console.log("destination: " + newDestination)
-        console.log("first: " + newFirst)
-        console.log("frequency: " + newFrequency)
-        console.log("next: " + newNext)
+
+        //push inputs to firebase
+        var newTrain = {
+            Name: newName,
+            Destination: newDestination,
+            First: newFirst,
+            Frequency: newFrequency,
+        }
+        database.ref().push(newTrain)
 
         //Clear search boxes
         $("#newTrainName").val("")
@@ -50,9 +42,24 @@ $(document).ready(function () {
         $("#newTrainFirst").val("")
         $("#newTrainFrequency").val("")
         $("#newTrainNext").val("")
+    })
 
-        var nextDeparture = 0
+    //database on update child
+    database.ref().on("child_added", function (childSnapshot) {
+        var newName = childSnapshot.val().Name;
+        var newDestination = childSnapshot.val().Destination;
+        var newFirst = childSnapshot.val().First;
+        var newFrequency = childSnapshot.val().Frequency;
+        console.log("name: " + newName)
+        console.log("destination: " + newDestination)
+        console.log("first: " + newFirst)
+        console.log("frequency: " + newFrequency)
 
+        //next departure math
+        var nextDeparture = $("#newTrainFirst").val().trim()
+        var timeTest = moment(nextDeparture, "HH" + "mm").format('LT')
+
+        //append database values to page
         var newRow = $("<tr>").append(
             $("<td>").text(newName),
             $("<td>").text(newDestination),
